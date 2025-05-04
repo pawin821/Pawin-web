ce1dae576a7d502b2f820bfdba42c6615591bdbc
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2025-05-04 23:54:13 +0530

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { Heart, Search, MapPin, Video, PawPrint, Calendar, DollarSign, 
          MessageCircle, Star, Users, Briefcase, User, AlertTriangle } from 'lucide-react';
 import { Stethoscope } from "lucide-react";
